0eba188bd3a4ee951c941e81d1af5081d0826c51
Karol Werner <karol@ppkt.eu>
2017-06-18 12:52:24 +0200

    Second iteration from tutorial

--- django_channels/chat/consumers.py
+++ django_channels/chat/consumers.py
@@ -1,11 +1,9 @@
-from channels.handler import AsgiHandler
 from channels.message import Message
-from django.http import HttpResponse
 
 
-def http_consumer(message: Message):
-    response = HttpResponse(
-        f'Hello, World! You asked for ${message.content["path"]}')
-
-    for chunk in AsgiHandler.encode_response(response):
-        message.reply_channel.send(chunk)
+def ws_message(message: Message):
+    # ASGI WebSocket packet-received and send-packet message types
+    # both have a "text" key for their textual data
+    message.reply_channel.send({
+        'text': message.content['text'],
+    })
--- django_channels/django_channels/routing.py
+++ django_channels/django_channels/routing.py
@@ -1,5 +1,7 @@
 from channels.routing import route
 
+from chat.consumers import ws_message
+
 channel_routing = [
-    route('http.request', 'chat.consumers.http_consumer')
+    route('websocket.receive', ws_message),
 ]
