42a79b6ce277f0b74952e6d8e8ac2b0235a0a66b
Karol Werner <karol@ppkt.eu>
2017-06-18 13:17:24 +0200

    Add Channel Sessions

--- django_channels/chat/consumers.py
+++ django_channels/chat/consumers.py
@@ -1,23 +1,34 @@
 from channels import Group
 from channels.message import Message
+from channels.sessions import channel_session
 
 
 # Connected to websocket.connect
-def ws_add(message: Message):
-    # Accept the incoming connection
+@channel_session
+def ws_connect(message: Message):
+    # Accept connection
     message.reply_channel.send({'accept': True})
 
+    # Work out room name from path (ignore slashes)
+    room = message.content['path'].strip('/')
+
+    # Save room in session and add us to the group
+    message.channel_session['room'] = room
+
     # Add them to channel group
-    Group('chat').add(message.reply_channel)
+    Group(f'chat-{room}').add(message.reply_channel)
 
 
 # Connected to websocket.receive
+@channel_session
 def ws_message(message: Message):
-    Group('chat').send({
+    Group(f'chat-{message.channel_session["room"]}').send({
         'text': f'[user] {message.content["text"]}',
     })
 
 
 # Connected to websocket.disconnect
+@channel_session
 def ws_disconnect(message: Message):
-    Group('chat').discard(message.reply_channel)
+    Group(f'chat-{message.channel_session["room"]}').discard(
+        message.reply_channel)
