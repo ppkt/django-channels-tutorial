2de59e04b011134ccf20ccddbb0f900d762be344
Karol Werner <karol@ppkt.eu>
2017-06-18 13:00:22 +0200

    Third iteration from tutorial

--- django_channels/chat/consumers.py
+++ django_channels/chat/consumers.py
@@ -1,9 +1,23 @@
+from channels import Group
 from channels.message import Message
 
 
+# Connected to websocket.connect
+def ws_add(message: Message):
+    # Accept the incoming connection
+    message.reply_channel.send({'accept': True})
+
+    # Add them to channel group
+    Group('chat').add(message.reply_channel)
+
+
+# Connected to websocket.receive
 def ws_message(message: Message):
-    # ASGI WebSocket packet-received and send-packet message types
-    # both have a "text" key for their textual data
-    message.reply_channel.send({
-        'text': message.content['text'],
+    Group('chat').send({
+        'text': f'[user] {message.content["text"]}',
     })
+
+
+# Connected to websocket.disconnect
+def ws_disconnect(message: Message):
+    Group('chat').discard(message.reply_channel)
